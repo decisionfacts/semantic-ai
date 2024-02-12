802b4f2f1ff01e3b96571a9452fb953b670849d8
jeyachandranv <jeyachandran@decisionforce.io>
2024-02-12 13:04:28 +0530

    revice fixed

--- semantic_ai/connectors/mysql.py
+++ semantic_ai/connectors/mysql.py
@@ -6,7 +6,7 @@ from langchain.utilities import SQLDatabase
 from mysql.connector import errorcode
 
 from semantic_ai.connectors.base import BaseSqlConnector
-from semantic_ai.connectors.exceptions import ConnectorConfigError
+from semantic_ai.exceptions import ConnectorError
 from semantic_ai.llm import Openai
 from sqlalchemy.exc import OperationalError
 from semantic_ai.utils import sync_to_async
@@ -37,11 +37,11 @@ class Mysql(BaseSqlConnector):
             return await sync_to_async(SQLDatabase.from_uri, mysql_uri)
 
         except OperationalError as ex:
-            logger.error(f"Error connecting to the database:", exc_info=ex)
-            raise ConnectorConfigError(f"Error connecting to the database")
+            logger.error("`mysql` database error!", exc_info=ex)
+            raise ConnectorError(f"`mysql` database error!\n{ex}")
         except Exception as ex:
-            logger.error('Search query exception => ', exc_info=ex)
-            raise ConnectorConfigError(f"Error connecting to the database")
+            logger.error('`mysql` connector error!', exc_info=ex)
+            raise ConnectorError(f'`mysql` connector error!\n{ex}')
 
     async def mysql_client(self):
         try:
@@ -56,13 +56,13 @@ class Mysql(BaseSqlConnector):
         except mysql.connector.Error as err:
             if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
                 logger.error("Something is wrong with your user name or password")
-                raise ConnectorConfigError("Something is wrong with your user name or password")
+                raise ConnectorError("Something is wrong with your user name or password")
             elif err.errno == errorcode.ER_BAD_DB_ERROR:
                 logger.error("Database does not exist")
-                raise ConnectorConfigError("Database does not exist")
+                raise ConnectorError("Database does not exist")
             else:
                 logger.error(err)
-                raise ConnectorConfigError(err)
+                raise ConnectorError(err)
 
     async def execute(self, data: dict):
         try:
@@ -71,7 +71,7 @@ class Mysql(BaseSqlConnector):
             conn = await self.mysql_client()
             curr = await sync_to_async(conn.cursor)
             await sync_to_async(curr.execute, query)
-            labels = [description[0] for description in curr.description]
+            labels = [description[0] async for description in curr.description]
             response = await sync_to_async(curr.fetchall)
             curr.close()
             resp = {
@@ -88,8 +88,8 @@ class Mysql(BaseSqlConnector):
             if not llm_result or not llm_result.generations:
                 return resp
 
-            for generation in llm_result.generations:
-                for result in generation:
+            async for generation in llm_result.generations:
+                async for result in generation:
                     if result:
                         resp['result'] = f'{resp.get("result")}\n {result.text}'
                         resp['ref_type'] = 'table'
@@ -99,5 +99,5 @@ class Mysql(BaseSqlConnector):
 
         except Exception as ex:
             logger.error('Search query exception => ', exc_info=ex)
-            raise ConnectorConfigError(f"Error search query exception")
+            raise ConnectorError(f"Error search query exception")
 
--- semantic_ai/connectors/sqlite.py
+++ semantic_ai/connectors/sqlite.py
@@ -2,17 +2,15 @@ import json
 import logging
 import sqlite3
 
-
 from langchain.utilities import SQLDatabase
 
 from semantic_ai.connectors.base import BaseSqlConnector
 from semantic_ai.llm import Openai
 from semantic_ai.utils import sync_to_async
 from sqlalchemy.exc import OperationalError
-from semantic_ai.connectors.exceptions import ConnectorConfigError
+from semantic_ai.exceptions import ConnectorError
 
 logger = logging.getLogger(__name__)
-
 
 SQL_RESPONSE_TEMPLATE = """Generate natural language information based {question} and {response}.
 """
@@ -34,18 +32,18 @@ class Sqlite(BaseSqlConnector):
 
         except OperationalError as ex:
             logger.error(f"Error connecting to the database:", exc_info=ex)
-            raise ConnectorConfigError(f"Error connecting to the database")
+            raise ConnectorError(f"Error connecting to the database")
         except Exception as ex:
             logger.error('Error connecting to the database => ', exc_info=ex)
-            raise ConnectorConfigError(f"Error connecting to the database")
+            raise ConnectorError(f"Error connecting to the database")
 
     async def execute(self, data: dict):
         try:
             query = data.get('SQLQuery')
             question = data.get('Question')
-            conn = sqlite3.connect(self.sql_path)
-            curr = conn.execute(query)
-            labels = [description[0] for description in curr.description]
+            conn = await sync_to_async(sqlite3.connect, self.sql_path)
+            curr = await sync_to_async(conn.execute, query)
+            labels = [description[0] async for description in curr.description]
             response = curr.fetchall()
             resp = {
                 'query': question,
@@ -61,8 +59,8 @@ class Sqlite(BaseSqlConnector):
             if not llm_result or not llm_result.generations:
                 return resp
 
-            for generation in llm_result.generations:
-                for result in generation:
+            async for generation in llm_result.generations:
+                async for result in generation:
                     if result:
                         resp['result'] = f'{resp.get("result")}\n {result.text}'
                         resp['ref_type'] = 'table'
@@ -71,4 +69,4 @@ class Sqlite(BaseSqlConnector):
             return resp
         except Exception as ex:
             logger.error('Search query exception => ', exc_info=ex)
-            raise ConnectorConfigError(f"Error connecting to the database")
+            raise ConnectorError(f"Error connecting to the database")
